98b684c09c34ba1ef6b7ebb837c057433e48c684
Derick <derick1castro@gmail.com>
2023-09-08 10:36:53 -0300

    Como usar os verbos http get, post, put e delete

---
+++ 05_FirstStepsInJavaWithSpringBoot/src/main/java/com/example/demo/PersonController.java
@@ -0,0 +1,55 @@
+package com.example.demo;
+
+import com.example.demo.model.Person;
+import com.example.demo.services.PersonServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+    @Autowired
+    private PersonServices personServices;
+
+    @RequestMapping(method = RequestMethod.GET,
+        produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Person> findAll(){
+        return personServices.findAll();
+    }
+
+    @RequestMapping(value = "/{id}",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person findById(@PathVariable(value = "id") String id) {
+        return personServices.findById(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Person create(@RequestBody Person person) {
+        return personServices.create(person);
+    }
+    @RequestMapping(method = RequestMethod.PUT,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Person update(@RequestBody Person person) {
+        return personServices.update(person);
+    }
+    @RequestMapping(value = "/{id}",
+            method = RequestMethod.DELETE)
+    public void delete(@PathVariable(value = "id") String id) {
+         personServices.delete(id);
+    }
+
+
+
+
+
+}
